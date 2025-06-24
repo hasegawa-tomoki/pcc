0c404f0748619228677894d7014d7e83a1ec1d8c
hasegawa-tomoki <github@speedstars.jp>
2025-06-25 04:50:43 +0900

    Copy from chibicc

--- ctests/TestCase/test.h
+++ ctests/TestCase/test.h
@@ -1,6 +1,6 @@
 #define ASSERT(x, y) assert(x, y, #y)
 
-int assert(int expected, int actual, char *code);
+void assert(int expected, int actual, char *code);
 int printf(char *fmt, ...);
 int sprintf(char *buf, char *fmt, ...);
 int strcmp(char *p, char *q);
