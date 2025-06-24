1fd470261fd0c06677c360cf5fc8e24445f89678
hasegawa-tomoki <github@speedstars.jp>
2025-06-25 04:26:40 +0900

    Fix case

--- ctests/TestCase/extern.c
+++ ctests/TestCase/extern.c
@@ -8,13 +8,13 @@ int main() {
   ASSERT(5, *ext2);
 
   extern int ext3;
-  assert(7, ext3);
+  ASSERT(7, ext3);
 
   int ext_fn1(int x);
-  assert(5, ext_fn1(5));
+  ASSERT(5, ext_fn1(5));
 
   extern int ext_fn2(int x);
-  assert(8, ext_fn2(8));
+  ASSERT(8, ext_fn2(8));
 
   printf("OK\n");
   return 0;
