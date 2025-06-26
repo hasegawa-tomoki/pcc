b0f23d8f5f13cddca01d0a072544beb5ce230dd0
hasegawa-tomoki <github@speedstars.jp>
2025-06-26 17:28:47 +0900

    Allow multibyte UTF-8 character in identifier

--- src/Tokenizer/Tokenizer.php
+++ src/Tokenizer/Tokenizer.php
@@ -100,12 +100,33 @@ class Tokenizer
 
     public function isIdent1(string $c): bool
     {
-        return preg_match('/^[a-zA-Z_]/', $c);
+        if (strlen($c) === 1) {
+            $ord = ord($c);
+            return ($ord >= ord('a') && $ord <= ord('z')) || 
+                   ($ord >= ord('A') && $ord <= ord('Z')) || 
+                   $ord === ord('_');
+        }
+        
+        // For multibyte characters, decode UTF-8 and check Unicode ranges
+        [$codepoint, ] = self::decodeUtf8($c, 0);
+        return self::isIdent1Unicode($codepoint);
     }
 
     public function isIdent2(string $c): bool
     {
-        return $this->isIdent1($c) or preg_match('/^[0-9]/', $c);
+        if (strlen($c) === 1) {
+            $ord = ord($c);
+            if (($ord >= ord('0') && $ord <= ord('9'))) {
+                return true;
+            }
+            return ($ord >= ord('a') && $ord <= ord('z')) || 
+                   ($ord >= ord('A') && $ord <= ord('Z')) || 
+                   $ord === ord('_');
+        }
+        
+        // For multibyte characters, decode UTF-8 and check Unicode ranges
+        [$codepoint, ] = self::decodeUtf8($c, 0);
+        return self::isIdent2Unicode($codepoint);
     }
 
     public function stringLiteralEndPos(int $pos): int
@@ -805,17 +826,15 @@ class Tokenizer
             }
 
             // Identifier or keyword
-            if ($this->isIdent1($this->currentInput[$pos])){
-                $start = $pos;
-                while ($pos < strlen($this->currentInput) && $this->isIdent2($this->currentInput[$pos])){
-                    $pos++;
-                }
-                $token = new Token(TokenKind::TK_IDENT, substr($this->currentInput, $start, $pos - $start), $start);
+            $identLen = $this->readIdent($pos);
+            if ($identLen) {
+                $token = new Token(TokenKind::TK_IDENT, substr($this->currentInput, $pos, $identLen), $pos);
                 $token->atBol = $atBol;
                 $token->hasSpace = $hasSpace;
                 $token->file = $this->currentFile;
                 $atBol = $hasSpace = false;
                 $tokens[] = $token;
+                $pos += $identLen;
                 continue;
             }
 
@@ -1095,5 +1114,79 @@ class Tokenizer
         $this->currentFile = new File('<built-in>', 1, $input);
         $this->tokenize();
         return $this->tok;
+    }
+
+    // Read an identifier and returns the length of it.
+    // If p does not point to a valid identifier, 0 is returned.
+    private function readIdent(int $start): int
+    {
+        $pos = $start;
+        [$c, $pos] = self::decodeUtf8($this->currentInput, $pos);
+        if (!self::isIdent1Unicode($c)) {
+            return 0;
+        }
+
+        while ($pos < strlen($this->currentInput)) {
+            [$c, $newPos] = self::decodeUtf8($this->currentInput, $pos);
+            if (!self::isIdent2Unicode($c)) {
+                return $pos - $start;
+            }
+            $pos = $newPos;
+        }
+        
+        return $pos - $start;
+    }
+
+    private static function inRange(array $range, int $c): bool
+    {
+        for ($i = 0; $i < count($range); $i += 2) {
+            if ($range[$i] === -1) {
+                break;
+            }
+            if ($range[$i] <= $c && $c <= $range[$i + 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // [https://www.sigbus.info/n1570#D] C11 allows not only ASCII but
+    // some multibyte characters in certain Unicode ranges to be used in an
+    // identifier.
+    //
+    // This function returns true if a given character is acceptable as
+    // the first character of an identifier.
+    private static function isIdent1Unicode(int $c): bool
+    {
+        static $range = [
+            ord('_'), ord('_'), ord('a'), ord('z'), ord('A'), ord('Z'),
+            0x00A8, 0x00A8, 0x00AA, 0x00AA, 0x00AD, 0x00AD, 0x00AF, 0x00AF,
+            0x00B2, 0x00B5, 0x00B7, 0x00BA, 0x00BC, 0x00BE, 0x00C0, 0x00D6,
+            0x00D8, 0x00F6, 0x00F8, 0x00FF, 0x0100, 0x02FF, 0x0370, 0x167F,
+            0x1681, 0x180D, 0x180F, 0x1DBF, 0x1E00, 0x1FFF, 0x200B, 0x200D,
+            0x202A, 0x202E, 0x203F, 0x2040, 0x2054, 0x2054, 0x2060, 0x206F,
+            0x2070, 0x20CF, 0x2100, 0x218F, 0x2460, 0x24FF, 0x2776, 0x2793,
+            0x2C00, 0x2DFF, 0x2E80, 0x2FFF, 0x3004, 0x3007, 0x3021, 0x302F,
+            0x3031, 0x303F, 0x3040, 0xD7FF, 0xF900, 0xFD3D, 0xFD40, 0xFDCF,
+            0xFDF0, 0xFE1F, 0xFE30, 0xFE44, 0xFE47, 0xFFFD,
+            0x10000, 0x1FFFD, 0x20000, 0x2FFFD, 0x30000, 0x3FFFD, 0x40000, 0x4FFFD,
+            0x50000, 0x5FFFD, 0x60000, 0x6FFFD, 0x70000, 0x7FFFD, 0x80000, 0x8FFFD,
+            0x90000, 0x9FFFD, 0xA0000, 0xAFFFD, 0xB0000, 0xBFFFD, 0xC0000, 0xCFFFD,
+            0xD0000, 0xDFFFD, 0xE0000, 0xEFFFD, -1,
+        ];
+
+        return self::inRange($range, $c);
+    }
+
+    // Returns true if a given character is acceptable as a non-first
+    // character of an identifier.
+    private static function isIdent2Unicode(int $c): bool
+    {
+        static $range = [
+            ord('0'), ord('9'), 0x0300, 0x036F, 0x1DC0, 0x1DFF, 0x20D0, 0x20FF,
+            0xFE20, 0xFE2F, -1,
+        ];
+
+        return self::isIdent1Unicode($c) || self::inRange($range, $c);
     }
 }
