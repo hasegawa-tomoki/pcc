c542e373b18bdd9df2b0e85a5f931baed68bc132
hasegawa-tomoki <github@speedstars.jp>
2025-06-26 17:23:01 +0900

    Define __STDC_UTF_{16,32}__ macros

--- src/Preprocessor/Preprocessor.php
+++ src/Preprocessor/Preprocessor.php
@@ -1276,6 +1276,8 @@ class Preprocessor
         self::defineMacro('__STDC_NO_COMPLEX__', '1');
         self::defineMacro('__STDC_NO_THREADS__', '1');
         self::defineMacro('__STDC_NO_VLA__', '1');
+        self::defineMacro('__STDC_UTF_16__', '1');
+        self::defineMacro('__STDC_UTF_32__', '1');
         self::defineMacro('__STDC_VERSION__', '201112L');
         self::defineMacro('__STDC__', '1');
         self::defineMacro('__USER_LABEL_PREFIX__', '');
