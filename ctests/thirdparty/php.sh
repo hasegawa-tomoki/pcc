9bae73310c2c64fd90415cc8444c6e3de09fd5c4
hasegawa-tomoki <github@speedstars.jp>
2025-06-30 02:41:12 +0900

    Disable opcache

--- ctests/thirdparty/php.sh
+++ ctests/thirdparty/php.sh
@@ -4,6 +4,6 @@ repo='https://github.com/php/php-src.git'
 git reset --hard 8d116a4ba10703c54d947d95e152d25d75d45aa0
 
 CC=$pcc CFLAGS=-D_GNU_SOURCE ./buildconf
-CC=$pcc CFLAGS=-D_GNU_SOURCE ./configure
+CC=$pcc CFLAGS=-D_GNU_SOURCE ./configure --disable-opcache
 $make
 $make test
